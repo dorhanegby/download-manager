ac9df2868ae6fdc173a8c425b0ccc84e2fb89561
Dor <dor@chatleap.com>
2020-01-11 12:49:04 +0200

    DownaloderContext implemented

--- src/IdcDm.java
+++ src/IdcDm.java
@@ -1,6 +1,31 @@
+import java.io.*;
+import java.nio.*;
+import java.net.*;
+import java.util.*;
+
 public class IdcDm {
 
     public static void main(String[] args) {
-        System.out.println("Hey");
+        System.out.println("all good");
+//        try {
+//            URL url = new URL("https://www.w3.org/TR/PNG/iso_8859-1.txt");
+//            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+//            httpURLConnection.setRequestMethod("GET");
+//            System.out.println(httpURLConnection.getContentLengthLong());
+//
+//            try (BufferedInputStream in = new BufferedInputStream(url.openStream());
+//                 BufferedOutputStream fileOutputStream = new BufferedOutputStream(System.out)) {
+//                byte dataBuffer[] = new byte[1024];
+//                int bytesRead;
+//                while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+//                    fileOutputStream.write(dataBuffer, 10, 1000);
+//                }
+//            } catch (IOException e) {
+//                // handle exception
+//            }
+//        }
+//        catch (Exception e) {
+//            System.out.println(e.getMessage());
+//        }
     }
 }
---
+++ src/Range.java
@@ -0,0 +1,17 @@
+public class Range {
+    private int startByte;
+    private int endByte;
+
+    public Range(int startByte, int endByte) {
+        this.startByte = startByte;
+        this.endByte = endByte;
+    }
+
+    public int getStartByte() {
+        return startByte;
+    }
+
+    public int getEndByte() {
+        return endByte;
+    }
+}
